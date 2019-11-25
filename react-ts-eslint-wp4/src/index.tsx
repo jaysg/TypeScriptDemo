4d33f653df8f3816417e2d9859f16573ee7a702e
jaysg <a8022476@yeah.net>
2019-11-25 15:09:18 +0800

    upd: 合并app.tsx和index.tsx

--- react-ts-eslint-wp4/src/index.tsx
+++ react-ts-eslint-wp4/src/index.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from '@/App';
-import '@/style.css';
+import { BrowserRouter } from 'react-router-dom';
+import renderRoutes from '@/utils/renderRouters';
+import { routes, basename } from './router';
+import '@/index.less';
+const authed = false;
+const authPath = '/login';
+
+const App: React.FC = () => {
+  return <BrowserRouter basename={basename}>{renderRoutes(routes, authed, authPath)}</BrowserRouter>;
+};
 
 ReactDOM.render(<App />, document.getElementById('root'));
